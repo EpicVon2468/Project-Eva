6a2b38ee5429c913fb0d0a211be367acab7d9594
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2025-03-10 14:15:25 +1100

    Expanded AT Field hitboxes slightly, this seems to fix the issue of all other entities but the player and projectiles ignoring collision.

--- src/main/java/wiiu/mavity/project_eva/entity/custom/ATFieldEntity.kt
+++ src/main/java/wiiu/mavity/project_eva/entity/custom/ATFieldEntity.kt
@@ -19,11 +19,22 @@ class ATFieldEntity(entityType: EntityType<out ATFieldEntity>, world: World) : E
 
     override fun writeCustomDataToNbt(nbt: NbtCompound) {}
 
-    override fun canHit(): Boolean = !this.isRemoved
+    override fun canBeHitByProjectile(): Boolean = true
+
+    override fun canHit(): Boolean = true
 
     override fun isCollidable(): Boolean = true
 
-    override fun isPushable(): Boolean = false
+    override fun collidesWith(other: Entity): Boolean {
+        // We can implement logic for if the other is an AT Field entity here :O
+        return true
+    }
+
+    override fun canUsePortals(): Boolean = false
+
+    override fun canAvoidTraps(): Boolean = true
+
+    override fun hasNoGravity(): Boolean = true
 
     override fun calculateBoundingBox(): Box {
         val x = this.x
@@ -32,8 +43,8 @@ class ATFieldEntity(entityType: EntityType<out ATFieldEntity>, world: World) : E
         val width = (this.width + 3.0f) * AT_FIELD_SIZE_MODIFIER
         val height = (this.height + 3.0f) * AT_FIELD_SIZE_MODIFIER
         return when (this.horizontalFacing) {
-            Direction.SOUTH, Direction.NORTH -> Box(x - width, y, z, x + width, y + height, z)
-            Direction.WEST, Direction.EAST -> Box(x, y, z - width, x, y + height, z + width)
+            Direction.SOUTH, Direction.NORTH -> Box(x - width, y, z, x + width, y + height, z).expand(0.0, 0.0, 0.1)
+            Direction.WEST, Direction.EAST -> Box(x, y, z - width, x, y + height, z + width).expand(0.1, 0.0, 0.0)
             else -> Box(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
         }
     }
