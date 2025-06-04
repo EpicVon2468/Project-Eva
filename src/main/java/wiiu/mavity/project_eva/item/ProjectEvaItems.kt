c124af273b2baee049ab3ac10606b9ea8835c16c
AngerVon2468 <thomaschad0909@gmail.com>
2025-06-04 13:05:43 +1000

    .

--- src/main/java/wiiu/mavity/project_eva/item/ProjectEvaItems.kt
+++ src/main/java/wiiu/mavity/project_eva/item/ProjectEvaItems.kt
@@ -1,5 +1,7 @@
 package wiiu.mavity.project_eva.item
 
+import com.oroarmor.multiitemlib.api.UniqueItemRegistry
+
 import net.minecraft.item.Item
 import net.minecraft.registry.*
 import net.minecraft.util.Identifier
@@ -13,9 +15,13 @@ object ProjectEvaItems {
         ProjectEva.LOGGER.info("Registering Items for Project Eva!")
     }
 
-    val SPEAR_LONGINUS = this.registerItem("spear_longinus", BaseSpear())
+    val SPEAR_LONGINUS = this.registerItem("spear_longinus", BaseSpear()).also {
+        UniqueItemRegistry.TRIDENT.addItemToRegistry(it)
+    }
 
-    val SPEAR_CASSIUS = this.registerItem("spear_cassius", BaseSpear())
+    val SPEAR_CASSIUS = this.registerItem("spear_cassius", BaseSpear()).also {
+        UniqueItemRegistry.TRIDENT.addItemToRegistry(it)
+    }
 
     fun <T : Item> registerItem(name: String, item: T): T = Registry.register(Registries.ITEM, Identifier(ProjectEva.MOD_ID, name), item)
 }
