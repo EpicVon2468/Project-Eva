7beaa0b60fae3c0c1ddcd2cd6b802bc12d8a2b8d
AngerVon2468 <thomaschad0909@gmail.com>
2025-06-15 16:56:32 +1000

    .

--- src/main/java/wiiu/mavity/project_eva/item/custom/BaseSpear.kt
+++ src/main/java/wiiu/mavity/project_eva/item/custom/BaseSpear.kt
@@ -12,25 +12,23 @@ import net.minecraft.world.World
 class BaseSpear : TridentItem(Settings()) {
 
     override fun onStoppedUsing(stack: ItemStack, world: World, user: LivingEntity, remainingUseTicks: Int) {
-        if (user !is PlayerEntity) return
-        if (!world.isClient) {
-            val isCreative = user.abilities.creativeMode
-            val tridentEntity = TridentEntity(world, user, stack)
-            tridentEntity.setVelocity(user, user.pitch, user.yaw, 0.0f, 5.0f, 0.0f)
-            if (isCreative) tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY
-            world.spawnEntity(tridentEntity)
-            world.playSoundFromEntity(
-                null,
-                tridentEntity,
-                SoundEvents.ITEM_TRIDENT_THROW,
-                SoundCategory.PLAYERS,
-                1.0f,
-                1.0f
-            )
-            if (!isCreative) user.inventory.removeOne(stack)
-            user.incrementStat(Stats.USED.getOrCreateStat(this))
-        }
-    }
+        if (user !is PlayerEntity || world.isClient) return
+		val isCreative = user.abilities.creativeMode
+		val tridentEntity = TridentEntity(world, user, stack)
+		tridentEntity.setVelocity(user, user.pitch, user.yaw, 0.0f, 5.0f, 0.0f)
+		if (isCreative) tridentEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY
+		world.spawnEntity(tridentEntity)
+		world.playSoundFromEntity(
+			null,
+			tridentEntity,
+			SoundEvents.ITEM_TRIDENT_THROW,
+			SoundCategory.PLAYERS,
+			1.0f,
+			1.0f
+		)
+		if (!isCreative) user.inventory.removeOne(stack)
+		user.incrementStat(Stats.USED.getOrCreateStat(this))
+	}
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         user.setCurrentHand(hand)
